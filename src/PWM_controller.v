cb4af9f9db10e4d1ecdedd4e4d4defd2053e8897
DomnikBrandstetter <154206435+DomnikBrandstetter@users.noreply.github.com>
2023-12-23 18:32:05 +0100

    Update PWM_controller.v

--- src/PWM_controller.v
+++ src/PWM_controller.v
@@ -1,4 +1,18 @@
-module Fan_PWM_controller #(parameter COUNTER_BITWIDTH=8)(
+// Copyright 2023 Dominik Brandstetter
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE−2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+module PWM_controller #(parameter COUNTER_BITWIDTH)(
     input wire clk_i,
     input wire clk_en_i,
     input wire rstn_i,
@@ -11,13 +25,15 @@ module Fan_PWM_controller #(parameter COUNTER_BITWIDTH=8)(
 reg [COUNTER_BITWIDTH:0] counterValue;
 reg [COUNTER_BITWIDTH:0] periodCounterValue;
 reg [COUNTER_BITWIDTH:0] counter;
+reg [COUNTER_BITWIDTH-1:0] minCounterValue;
 
-always @(posedge clk_i) begin
+always @(posedge clk_i, rstn_i) begin
 
     if (!rstn_i) begin
         counter <= 0;
         counterValue <= 0;
         periodCounterValue <= 0;
+        minCounterValue <= 0;
     end else if (clk_en_i && counter == periodCounterValue) begin
         counter <= 0;
         periodCounterValue <= periodCounterValue_i;
