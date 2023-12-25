73e6534b8550aec0581a13dc80149e4abbc8ae46
DomnikBrandstetter <154206435+DomnikBrandstetter@users.noreply.github.com>
2023-12-25 15:06:06 +0100

    Update PWM_controller.v

--- src/PWM_controller.v
+++ src/PWM_controller.v
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-module PWM_controller #(parameter COUNTER_BITWIDTH = 10)(
+module PWM_controller #(parameter COUNTER_BITWIDTH)(
     input wire clk_i,
     input wire clk_en_i,
     input wire rstn_i,
@@ -25,13 +25,15 @@ module PWM_controller #(parameter COUNTER_BITWIDTH = 10)(
 reg [COUNTER_BITWIDTH:0] counterValue;
 reg [COUNTER_BITWIDTH:0] periodCounterValue;
 reg [COUNTER_BITWIDTH:0] counter;
+reg [COUNTER_BITWIDTH-1:0] minCounterValue;
 
-always @(posedge clk_i) begin
+always @(posedge clk_i, rstn_i) begin
 
     if (!rstn_i) begin
         counter <= 0;
         counterValue <= 0;
         periodCounterValue <= 0;
+        minCounterValue <= 0;
     end else if (clk_en_i && counter == periodCounterValue) begin
         counter <= 0;
         periodCounterValue <= periodCounterValue_i;
@@ -41,7 +43,7 @@ always @(posedge clk_i) begin
     end
 end
 
-assign PWM_pin_o = (counter < counterValue || counter == periodCounterValue ) ? 1 : 0;
+assign PWM_pin_o = (counter <= counterValue && counterValue != 0) ? 1 : 0;
 
 endmodule
 
