07ac5afd71cc95acd425caee046712062935553a
DomnikBrandstetter <154206435+DomnikBrandstetter@users.noreply.github.com>
2023-12-29 15:45:07 +0100

    Update tt_um_FanCTRL_tb.v

--- src/tt_um_FanCTRL_tb.v
+++ src/tt_um_FanCTRL_tb.v
@@ -2,31 +2,16 @@
 `default_nettype none
 `include "tt_um_FanCTRL.v"
 
-module tt_um_FanCTRL_tb;
-
-    // this part dumps the trace to a vcd file that can be viewed with GTKWave
-    initial begin
-        $dumpfile ("tb.vcd");
-        $dumpvars (0, tt_um_FanCTRL_tb);
-        #1;
-    end
-
-// //PT2-Glied 8-Bit PID 100Hz
-// localparam ADC_BITWIDTH = 8;
-// localparam real B2_PT2 = 5.76318539197624e-06;   
-// localparam real B1_PT2 = 1.152637078395248e-05;  
-// localparam real B0_PT2 = 5.76318539197624e-06;  
-// localparam real A1_PT2 = -1.9961897885309716;
-// localparam real A0_PT2 = 0.9961920938051285;
+module tt_FanCTRL_tb;
 
 //PT2-Glied 4-Bit PI 1Hz 
 localparam ADC_BITWIDTH = 4;
-localparam PT2_FREQ = 5; // 5 Hz
-localparam real B2_PT2 = 0.0010627042223499215;   
-localparam real B1_PT2 = 0.002125408444699843;  
-localparam real B0_PT2 = 0.0010627042223499215;  
-localparam real A1_PT2 = -1.8591120801803738;
-localparam real A0_PT2 = 0.8599622435582536;
+localparam PT2_FREQ = 1000; // 5 Hz
+localparam real B2_PT2 = 1.3681259298752577e-07;   
+localparam real B1_PT2 = 2.7362518597505155e-07;  
+localparam real B0_PT2 = 1.3681259298752577e-07;  
+localparam real A1_PT2 = -1.9984103411771883;
+localparam real A0_PT2 = 0.9984104506272623;
 
 reg clk_tb = 0;
 reg rstn_tb = 0;
@@ -49,6 +34,7 @@ localparam PID_STAGES = 5; // 5 Hz
 localparam CLK_DIV_PID = 2;
 localparam PT2_CLK_DIV = $rtoi(CLK_EN_FREQ / (PT2_FREQ)) - 1;
 localparam PID_COUNTER_BITWIDTH = $rtoi(log2(PT2_CLK_DIV+1)); 
+localparam PT2_START_VAL = 50.0;
 reg [PID_COUNTER_BITWIDTH-1:0] clkCounterValue;
 
 wire [7:0] uio_oe;
@@ -61,16 +47,17 @@ assign PID_Val_tb = uio_out[ADC_BITWIDTH:0];
 
 assign ADC_VAL_sim = (PT2_enable == 1)? ADC_PT2_sim : ADC_TEST_sim;
 assign SET_VAL_sim = (PT2_enable == 1)? SET_PT2_sim : SET_TEST_sim; 
-assign ADC_PT2_sim = (PT2_val[0] < 0)? 0 : $rtoi(PT2_val[0]/5 + 0.5);
+assign ADC_PT2_sim = $rtoi(PT2_val[0]/5 + PT2_START_VAL/5);
 
 assign ui_in = {SET_VAL_sim, ADC_VAL_sim};  
 
+//simulation PT2
 always @(posedge clk_tb, rstn_tb) begin
     if (!rstn_tb) begin
         PID_val[0] <= 0.0;  
         PID_val[1] <= 0.0;    
-        PT2_val[0] <= 50.0;
-        PT2_val[1] <= 50.0;
+        PT2_val[0] <= 0.0;
+        PT2_val[1] <= 0.0;
     end else if(clkCounterValue == PT2_CLK_DIV) begin 
         PID_val[0] <= ($itor(PID_Val_tb));  
         PID_val[1] <= PID_val[0];
@@ -79,7 +66,7 @@ always @(posedge clk_tb, rstn_tb) begin
     end
 end
 
-tt_um_FanCTRL_DomnikBrandstetter #() tt_um_FAN (
+tt_um_FanCTRL #() tt_um_FAN (
     `ifdef GL_TEST
         .VPWR( 1'b1),
         .VGND( 1'b0),
@@ -89,10 +76,12 @@ tt_um_FanCTRL_DomnikBrandstetter #() tt_um_FAN (
     .uio_in (uio_in),    // IOs: Bidirectional Input path  - unused
     .uio_out (uio_out),  // IOs: Bidirectional Output path - unused
     .uio_oe (uio_oe),    // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
-    .ena (1'b1),          // will go high when the design is enabled
+    .ena (1'b1),         // will go high when the design is enabled
     .clk (clk_tb),       // clock - 1 MHz
     .rst_n (rstn_tb)     // reset_n - low to reset
 );
+
+always #1 clk_tb = ~clk_tb;
 
 always @(posedge clk_tb) begin
 
@@ -105,18 +94,6 @@ always @(posedge clk_tb) begin
     end
 end
 
-// always@(*) begin
-//     if (!rstn_tb) begin
-//          SET_PT2_sim <= 2;
-//     end else if(ADC_PT2_sim >= 12) begin
-//         SET_PT2_sim <= 2;
-//      end else if(ADC_PT2_sim <= 3) begin
-//         SET_PT2_sim <= 13;
-//     end 
-// end 
-
-always #1 clk_tb = ~clk_tb;
-
 initial begin
     ADC_TEST_sim = 7;
     SET_TEST_sim = 5;
@@ -125,15 +102,14 @@ initial begin
     #11;
     rstn_tb = 1;
 
-    //#100000000;
-    #10000
+    #100000000;
 
     PT2_enable = 1;
     rstn_tb = 0;
     #10;
     rstn_tb = 1; 
-    //#150000000 $finish;
-    #30000 $finish;
+    #150000000; 
+    //#30000 $finish;
 end
 
 function integer log2;
